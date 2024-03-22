7e2a9117a75ed15c9f342f800d33e857b1172513
Lucas de Melo Lima Oliveira <112504140+lmloCin@users.noreply.github.com>
2024-03-21 21:42:36 -0300

    Add files via upload

---
+++ Pc.py
@@ -0,0 +1,59 @@
+from socket import socket, AF_INET, SOCK_DGRAM
+from threading import Thread
+
+
+class Pc:
+    def __init__(self, pcnumber):
+        self.pcnumber = pcnumber
+        self.client = socket(AF_INET, SOCK_DGRAM)
+        self.servername = 'localhost'
+        self.serverport = ''
+        self.serverAddress = (self.servername, self.serverport)
+        self.nex = None
+        self.prev = None
+
+    def portchange(self, numberport):
+        self.serverport = int(numberport)
+        self.serverAddress = (self.servername, self.serverport)
+
+    def handlerequest(self, mserversocket):
+        message, clientaddress = self.client.recvfrom(2048)
+        req = message.decode()
+        print(f'Requisicao recebida de {clientaddress}')
+        print(f'A requisicao foi:{req}')
+        rep = f'Oi, aqui quem fala é {self.pcnumber}!'
+        mserversocket.sendto(rep.encode(), clientaddress)
+
+    def starserver(self):
+        self.client = socket(AF_INET, SOCK_DGRAM)
+        self.client.bind((self.servername, self.serverport))
+        print('The server is ready to receive')
+
+        for i in range(2):
+            Thread(target=self.handlerequest, args=(self.client,)).start()
+
+    def sendmessage(self, neighbor):
+        if neighbor == self.prev or neighbor == self.nex:
+
+            self.client = socket(AF_INET, SOCK_DGRAM)
+            while True:
+                message = input('>>')
+                if message == 'exit':
+                    self.client.close()
+                    break
+                self.client.sendto(message.encode(), self.serverAddress)
+                data, saddress = self.client.recvfrom(2048)
+                reply = data.decode()
+                print(f'Resposta recebida:{reply}')
+        else:
+            print('Conexão indisponivel')
+
+    def showpcnumber(self):
+        return self.pcnumber
+
+    def setnext_prev(self, chose1, chose2):
+        self.nex = chose1
+        self.prev = chose2
+
+    def showprev(self):
+        return self.prev.pcnumber
