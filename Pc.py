8ee5ccafc3f4bd61f56cf9ae9ac3fbaa90ab56d6
Lucas de Melo Lima Oliveira <112504140+lmloCin@users.noreply.github.com>
2024-03-22 05:04:51 -0300

    Update Pc.py
    criptografia

--- Pc.py
+++ Pc.py
@@ -1,5 +1,6 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 from threading import Thread
+import rsa
 
 
 class Pc:
@@ -13,6 +14,7 @@ class Pc:
         self.prev = None
         self.uc = uc
         self.key = None
+        self.otherkeys = []
 
         if not uc:
             self.serverport = 5000
@@ -20,7 +22,14 @@ class Pc:
             message = '.'
             self.client.sendto(message.encode(), self.serverAddress)
             data, saddress = self.client.recvfrom(2048)
-            self.key = data.decode()
+            new_data = data.decode().split(',')
+            a = int(new_data[0][11:])
+            b = int(new_data[1])
+            c = int(new_data[2])
+            d = int(new_data[3])
+            e = int(new_data[4][:72])
+
+            self.key = rsa.PrivateKey(a, b, c, d, e)
             self.client.close()
 
     def portchange(self, numberport):
@@ -29,14 +38,31 @@ class Pc:
 
     def handlerequest(self, mserversocket):
         message, clientaddress = self.client.recvfrom(2048)
+        req = message
+        print(req)
+        print(type(req))
+        print(self.key)
+        req = rsa.decrypt(message, self.key)
+        req = req.decode("utf-8")
+
+        print(f'Requisicao recebida de {clientaddress}')
+        print(f'A requisicao foi:{req}')
+        rep = f'Oi, aqui quem fala é {self.pcnumber}!'
+        mserversocket.sendto(rep.encode(), clientaddress)
+
+    def handlerequestuc(self, mserversocket):
+        message, clientaddress = self.client.recvfrom(2048)
         req = message.decode()
         if req == '.':
-            rep = 'chave'
+            pubkey, privkey = rsa.newkeys(512)
+            #print(pubkey)
+            #print(str(privkey))
+            self.otherkeys.append(pubkey)
+            rep = str(privkey)
             mserversocket.sendto(rep.encode(), clientaddress)
         else:
-            print(f'Requisicao recebida de {clientaddress}')
-            print(f'A requisicao foi:{req}')
-            rep = f'Oi, aqui quem fala é {self.pcnumber}!'
+            idx = int(req)
+            rep = str(self.otherkeys[idx - 1])
             mserversocket.sendto(rep.encode(), clientaddress)
 
     def starserver(self):
@@ -50,19 +76,41 @@ class Pc:
     def startuc(self):
         self.client = socket(AF_INET, SOCK_DGRAM)
         self.client.bind((self.servername, self.serverport))
-        for i in range(6):
-            Thread(target=self.handlerequest, args=(self.client,)).start()
+        for i in range(10):
+            Thread(target=self.handlerequestuc, args=(self.client,)).start()
+
+    def askforkey(self, needkey):
+        self.client = socket(AF_INET, SOCK_DGRAM)
+        self.serverport = 5000
+        self.serverAddress = (self.servername, self.serverport)
+        message = f'{needkey}'
+        self.client.sendto(message.encode(), self.serverAddress)
+        data, saddress = self.client.recvfrom(2048)
+        chave = data.decode()
+        self.client.close()
+        return chave
 
     def sendmessage(self, neighbor):
         if self.uc or neighbor == self.prev or neighbor == self.nex:
-
+            porta = self.serverport
+            chave = self.askforkey(neighbor.pcnumber).split(',')
+            chave1 = int(chave[0][10:])
+            chave2 = int(chave[1][:6])
+            chave = rsa.PublicKey(chave1, chave2)
+            print(chave)
+            self.serverport = porta
+            self.serverAddress = (self.servername, self.serverport)
             self.client = socket(AF_INET, SOCK_DGRAM)
             while True:
                 message = input('>>')
                 if message == 'exit':
                     self.client.close()
                     break
-                self.client.sendto(message.encode(), self.serverAddress)
+                data = message.encode()
+                message = rsa.encrypt(data, chave)
+                print(message)
+                print(type(message))
+                self.client.sendto(message, self.serverAddress)
                 data, saddress = self.client.recvfrom(2048)
                 reply = data.decode()
                 print(f'Resposta recebida:{reply}')
@@ -81,3 +129,6 @@ class Pc:
 
     def showkey(self):
         return self.key
+
+    def showpubkeys(self):
+        return self.otherkeys
