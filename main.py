b51ee1ffdb03609b65b2a54c298295eb411e211d
Lucas de Melo Lima Oliveira <112504140+lmloCin@users.noreply.github.com>
2024-03-21 21:39:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-import pc
+from Pc import Pc
 
-if __name__ == '__main__':
-    pc = pc.PC()
+pc1 = Pc(1)
+pc2 = Pc(2)
+pc3 = Pc(3)
+pc4 = Pc(4)
+pc5 = Pc(5)
+pc6 = Pc(6)
+pc1.setnext_prev(pc2, pc6)
+pc2.setnext_prev(pc3, pc1)
+pc3.setnext_prev(pc4, pc2)
+pc4.setnext_prev(pc5, pc3)
+pc5.setnext_prev(pc6, pc4)
+pc6.setnext_prev(pc1, pc5)
+listpc = [pc1, pc2, pc3, pc4, pc5, pc6]
 
-    pc.start_listening()
-
-    pc.send_messages_to_neighbors("hello")
+while True:
+    action = int(input('Qual PC pretende usar?'))
+    recive = int(input('Qual PC vai receber a mensagem?'))
+    listpc[recive - 1].portchange(action*1000 + recive)
+    listpc[recive - 1].starserver()
+    listpc[action - 1].portchange(action*1000 + recive)
+    listpc[action - 1].sendmessage(listpc[recive - 1])
