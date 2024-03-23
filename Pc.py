be5e5f4d8612c8eba77e82f6539c3117a8f5bf87
Severino Murilo <murilo91522608@gmail.com>
2024-03-22 22:24:32 -0300

    Added comments and docstrings in the code

--- Pc.py
+++ Pc.py
@@ -33,30 +33,69 @@ class Pc:
             self.client.close()
 
     def portchange(self, numberport):
+        """
+        Change the server port and address based on the input port number.
+
+        Args:
+        numberport (int): The new port number to set for the server.
+        """
         self.serverport = int(numberport)
+        """
+        Update the server address using the new port number.
+        """
         self.serverAddress = (self.servername, self.serverport)
 
-    def handlerequest(self, mserversocket):
+    def handle_request(self, mserversocket):
+        """
+        Handle the incoming request from the client.
+
+        Args:
+        self: the class instance
+        mserversocket: the main server socket
+
+        Returns:
+        None
+        """
+        # Receive the message and client address
         message, clientaddress = self.client.recvfrom(2048)
+
+        # Set the request to the received message
         req = message
+
+        # Print the request and its type
         print(req)
         print(type(req))
         print(self.key)
+
+        # Decrypt the message using the key and decode it to utf-8
         req = rsa.decrypt(message, self.key)
         req = req.decode("utf-8")
 
+        # Print the client address and the received request
         print(f'Requisicao recebida de {clientaddress}')
         print(f'A requisicao foi:{req}')
+
+        # Prepare and send the reply to the client
         rep = f'Oi, aqui quem fala é {self.pcnumber}!'
         mserversocket.sendto(rep.encode(), clientaddress)
 
-    def handlerequestuc(self, mserversocket):
+    def handle_request_uc(self, mserversocket):
+        """
+        Handle requests from a client by sending public/private key pairs.
+
+        Args:
+        - self: the instance of the class
+        - mserversocket: the socket to communicate with the client
+        """
+        # Receive message and client address
         message, clientaddress = self.client.recvfrom(2048)
         req = message.decode()
+
         if req == '.':
             pubkey, privkey = rsa.newkeys(512)
-            #print(pubkey)
-            #print(str(privkey))
+            # Uncomment if you want to print the public and private keys
+            # print(pubkey)
+            # print(str(privkey))
             self.otherkeys.append(pubkey)
             rep = str(privkey)
             mserversocket.sendto(rep.encode(), clientaddress)
@@ -65,42 +104,96 @@ class Pc:
             rep = str(self.otherkeys[idx - 1])
             mserversocket.sendto(rep.encode(), clientaddress)
 
-    def starserver(self):
+    def start_server(self):
+        """
+        Initialize the server and start listening for client requests.
+        """
         self.client = socket(AF_INET, SOCK_DGRAM)
         self.client.bind((self.servername, self.serverport))
         print('The server is ready to receive')
 
+        # Start two threads to handle client requests concurrently
         for i in range(2):
-            Thread(target=self.handlerequest, args=(self.client,)).start()
+            Thread(target=self.handle_request, args=(self.client,)).start()
 
     def startuc(self):
+        """
+        Initialize a client socket and start handling requests in a separate thread.
+        """
+        # Create a client socket
         self.client = socket(AF_INET, SOCK_DGRAM)
+
+        # Bind the client socket to the server name and port
         self.client.bind((self.servername, self.serverport))
+
+        # Start handling requests in separate threads
         for i in range(10):
             Thread(target=self.handlerequestuc, args=(self.client,)).start()
 
-    def askforkey(self, needkey):
+    def ask_for_key(self, need_key):
+        """
+        Sends a request for a key to the server and returns the key received.
+
+        Args:
+            need_key (str): The key that is needed.
+
+        Returns:
+            str: The key received from the server.
+        """
+        # Create a UDP socket
         self.client = socket(AF_INET, SOCK_DGRAM)
-        self.serverport = 5000
-        self.serverAddress = (self.servername, self.serverport)
-        message = f'{needkey}'
-        self.client.sendto(message.encode(), self.serverAddress)
-        data, saddress = self.client.recvfrom(2048)
-        chave = data.decode()
+
+        # Set the server port
+        self.server_port = 5000
+
+        # Set the server address
+        self.server_address = (self.server_name, self.server_port)
+
+        # Convert the key to a message string
+        message = f'{need_key}'
+
+        # Send the message to the server
+        self.client.sendto(message.encode(), self.server_address)
+
+        # Receive data and server address from the server
+        data, server_address = self.client.recvfrom(2048)
+
+        # Decode the received data to get the key
+        key = data.decode()
+
+        # Close the client socket
         self.client.close()
-        return chave
+
+        return key
 
     def sendmessage(self, neighbor):
+        """
+        Sends encrypted messages to a neighbor using RSA encryption.
+
+        Args:
+        - neighbor: The neighbor to whom the message will be sent
+
+        Returns:
+        - None
+        """
+        # Check if the connection is available and the neighbor is valid
         if self.uc or neighbor == self.prev or neighbor == self.nex:
+            # Store the server port for later use
             porta = self.serverport
+
+            # Retrieve the public key of the neighbor
             chave = self.askforkey(neighbor.pcnumber).split(',')
             chave1 = int(chave[0][10:])
             chave2 = int(chave[1][:6])
             chave = rsa.PublicKey(chave1, chave2)
             print(chave)
+
+            # Restore the original server port and address
             self.serverport = porta
             self.serverAddress = (self.servername, self.serverport)
             self.client = socket(AF_INET, SOCK_DGRAM)
+
+            # Start sending messages
             while True:
                 message = input('>>')
                 if message == 'exit':
@@ -113,16 +206,29 @@ class Pc:
                 self.client.sendto(message, self.serverAddress)
                 data, saddress = self.client.recvfrom(2048)
                 reply = data.decode()
-                print(f'Resposta recebida:{reply}')
+                print(f'Received response: {reply}')
         else:
-            print('Conexão indisponivel')
+            print('Connection unavailable')
 
-    def showpcnumber(self):
+    def show_pc_number(self):
+        """
+        Returns the pcnumber attribute of the object.
+
+        Returns:
+        str: The pcnumber attribute of the object.
+        """
         return self.pcnumber
 
-    def setnext_prev(self, chose1, chose2):
-        self.nex = chose1
-        self.prev = chose2
+    def set_next_prev(self, choice1, choice2):
+        """
+        Set the next and previous choices for the current node.
+
+        Args:
+        choice1: The next choice for the current node
+        choice2: The previous choice for the current node
+        """
+        self.next = choice1
+        self.prev = choice2
 
     def showprev(self):
         return self.prev.pcnumber
