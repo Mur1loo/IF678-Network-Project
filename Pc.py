5a183018eed3ba9d45770e1d6e951f1a72a16620
Severino Murilo <murilo91522608@gmail.com>
2024-03-22 22:31:12 -0300

    Update Pc.py

--- Pc.py
+++ Pc.py
@@ -1,8 +1,9 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 from threading import Thread
 
-
 class Pc:
+    """PC class for handling client-server communication over UDP."""
+    
     def __init__(self, pcnumber, uc):
         self.pcnumber = pcnumber
         self.client = socket(AF_INET, SOCK_DGRAM)
@@ -13,7 +14,6 @@ class Pc:
         self.prev = None
         self.uc = uc
         self.key = None
-
         if not uc:
             self.serverport = 5000
             self.serverAddress = (self.servername, self.serverport)
@@ -24,10 +24,22 @@ class Pc:
             self.client.close()
 
     def portchange(self, numberport):
+        """
+        Changes the server port of the PC.
+        
+        Args:
+        numberport (int): The new port number.
+        """
         self.serverport = int(numberport)
         self.serverAddress = (self.servername, self.serverport)
 
     def handlerequest(self, mserversocket):
+        """
+        Handles incoming requests on the server socket.
+        
+        Args:
+        mserversocket (socket): The main server socket to handle requests on.
+        """
         message, clientaddress = self.client.recvfrom(2048)
         req = message.decode()
         if req == '.':
@@ -40,22 +52,33 @@ class Pc:
             mserversocket.sendto(rep.encode(), clientaddress)
 
     def starserver(self):
+        """Starts the server to receive and handle requests."""
         self.client = socket(AF_INET, SOCK_DGRAM)
         self.client.bind((self.servername, self.serverport))
         print('The server is ready to receive')
 
+        # Start two threads to handle requests
         for i in range(2):
             Thread(target=self.handlerequest, args=(self.client,)).start()
 
     def startuc(self):
+        """Starts the user client to send and receive messages."""
         self.client = socket(AF_INET, SOCK_DGRAM)
         self.client.bind((self.servername, self.serverport))
+        
+        # Start six threads to handle requests for user clients
         for i in range(6):
             Thread(target=self.handlerequest, args=(self.client,)).start()
 
     def sendmessage(self, neighbor):
+        """
+        Sends a message to a neighbor or handles messages if the PC is a user client.
+        
+        Args:
+        neighbor (Pc): The neighbor PC to communicate with.
+        """
+        # Send message only if the PC is a user client or the neighbor is the previous or next PC
         if self.uc or neighbor == self.prev or neighbor == self.nex:
-
             self.client = socket(AF_INET, SOCK_DGRAM)
             while True:
                 message = input('>>')
@@ -70,14 +93,24 @@ class Pc:
             print('Conexão indisponivel')
 
     def showpcnumber(self):
+        """Returns the PC number."""
         return self.pcnumber
 
     def setnext_prev(self, chose1, chose2):
+        """
+        Sets the next and previous PCs.
+        
+        Args:
+        chose1 (Pc): The next PC.
+        chose2 (Pc): The previous PC.
+        """
         self.nex = chose1
         self.prev = chose2
 
     def showprev(self):
+        """Returns the previous PC number."""
         return self.prev.pcnumber
 
     def showkey(self):
+        """Returns the key if there is one."""
         return self.key
